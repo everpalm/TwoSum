3886ae62445be36ae6262c74b4f12719dd8bd8c0
Jaron <everpalm@gmail.com>
2019-07-30 22:57:41 +0800

    CTJ1907302257 Improve speed

--- test.py
+++ test.py
@@ -1,21 +1,9 @@
 class Solution:
     def twoSum(self, nums, target):
-        #print("target = ", target)
-        nums_len = len(nums)
-        nums_count = 0
-
-        for nums_count in range(nums_len):
-            x = nums[nums_count:nums_len]
-            print("x = ", x)
-            #print("nums_len = ", nums_len)
-            x_len = len(x)
-            for x_count in range(1, x_len, 1):
-                #print("x_count = ", x_count)
-                my_target = x[0] + x[x_count]
-
-                print("my_target = {}".format(my_target))
-                if my_target == target:
-                    return [nums_count, x_count+nums_count]
+        for nums_count in range(len(nums)):
+            for x_count in range(nums_count+1, len(nums), 1):
+                if target == nums[x_count] + nums[nums_count]:
+                    return [nums_count, x_count]
 
 if __name__ == "__main__":
     my_solution = Solution
