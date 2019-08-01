25972b0cf7cb7f99920dd350e6e97bb93c558f07
Jaron <everpalm@gmail.com>
2019-08-01 22:37:26 +0800

    CTJ1908012237 Add test case

--- test.py
+++ test.py
@@ -3,16 +3,17 @@ class Solution:
         complement={}
         for nums_count in range(len(nums)):
             if (target-nums[nums_count]) not in complement:
-                print(target, "-nums[",nums_count,"] = ", target-nums[nums_count])
+                #print("complement =", complement)
+                #print(target, "- nums[",nums_count,"] = ", target-nums[nums_count])
                 complement[nums[nums_count]]=nums_count
-                print("1. ","complement[",nums[nums_count],"] = ", nums_count)
+            #print("1. ","complement[",nums[nums_count],"] = ", nums_count)
             else:
-                print(target, "-nums[",nums_count,"] = ", target-nums[nums_count])
-                print("2. ","dic[",target-nums[nums_count],"] = ", nums_count)
+                #print(target, "- nums[",nums_count,"] = ", target-nums[nums_count])
+                #print("2. ","dic[",target-nums[nums_count],"] = ", nums_count)
                 return [complement[target-nums[nums_count]],nums_count]
 
 if __name__ == "__main__":
     my_solution = Solution
-    my_output = my_solution().twoSum([-1,-2,-3,-4,-5], -3)
+    my_output = my_solution().twoSum([1,2,3], 3)
     print(my_output)
 
---
+++ test_twosum.py
@@ -0,0 +1,8 @@
+import unittest
+from test import Solution
+
+class test_module(unittest.TestCase):
+    def test_twoSum(self):
+        self.assertEqual(Solution().twoSum([1,2,3], 3), [0,1])
+
+if __name__ == "__main__":    unittest.TextTestRunner(verbosity=3).run(unittest.TestLoader().loadTestsFromTestCase(test_module))
